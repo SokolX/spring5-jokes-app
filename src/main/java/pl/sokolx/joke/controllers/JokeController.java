213409e5ae78812eb7bd01541046ab00570eebd6
SokolX <89soek@gmail.com>
2018-11-18 21:34:31 +0100

    Added chucknorris.html

--- src/main/java/pl/sokolx/joke/controllers/JokeController.java
+++ src/main/java/pl/sokolx/joke/controllers/JokeController.java
@@ -1,10 +1,12 @@
 package pl.sokolx.joke.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.sokolx.joke.services.JokeService;
 
+@Controller
 public class JokeController {
 
     private JokeService jokeService;
